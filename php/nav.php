7725db328ee97ecd3aae52294a8a86754c1a022a
MathiasCabrol <mathiascabrol@gmail.com>
2022-01-12 16:56:23 +0100

    Correction erreur de balise html

--- php/nav.php
+++ php/nav.php
@@ -1,12 +1,13 @@
 <nav>
-    <ul>
-      <li class="menu-deroulant">
-        <a href="#">Menu rapide</a>
-        <ul class="sous-menu">
-          <li><a href="index.php">Accueil</a></li>
-          <li><a href="projects.php">Portfolio</a></li>
-          <li><a href="skills.php">Compétences</a></li>
-          <li><a href="contact.php">Contact</a></li>
-        </ul>
-      </li>
-  </nav>
+  <ul>
+    <li class="menu-deroulant">
+      <a href="#">Menu rapide</a>
+      <ul class="sous-menu">
+        <li><a href="index.php">Accueil</a></li>
+        <li><a href="projects.php">Portfolio</a></li>
+        <li><a href="skills.php">Compétences</a></li>
+        <li><a href="contact.php">Contact</a></li>
+      </ul>
+    </li>
+  </ul>
+</nav>
