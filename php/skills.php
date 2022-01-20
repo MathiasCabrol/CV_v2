282f3028ba9768d7971b376ef1c3fa701aebc16d
MathiasCabrol <mathiascabrol@gmail.com>
2022-01-20 17:00:02 +0100

    Création de bandeaux avec les logos de technos front et back-end sur page compétences

--- php/skills.php
+++ php/skills.php
@@ -5,9 +5,11 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css" integrity="sha512-Fo3rlrZj/k7ujTnHg4CGR2D7kSs0v4LLanw2qksYuRlEzO+tcaEPQogQ0KaoGN26/zrn20ImR1DfuLWnOo7aBA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
     <link rel="stylesheet" href="../assets/css/nav.css">
     <link rel="stylesheet" href="../assets/css/skills.css">
+    <link rel="stylesheet" href="../assets/css/caroussel.css">
     <title>Document</title>
 </head>
 
@@ -21,12 +23,33 @@
             <div>
                 <h1 class="skillsTitle">Développeur web</h1>
                 <div class="scrollButton">
-                <a href="#mainContainer" class="scrollIcon">></a>
+                    <a href="#mainContainer" class="scrollIcon">></a>
+                </div>
             </div>
+        </div>
+        <div class="row">
+            <div class="col-12 text-center">
+                <h2 class="skillsSubTitle mt-4">Front-End</h2>
+            </div>
+            <div class="col-12 text-center iconsCol mb-5">
+            <i class="fab fa-html5 fa-5x"></i>
+            <i class="fab fa-css3-alt fa-5x"></i>
+            <i class="fab fa-bootstrap fa-5x"></i>
+            <i class="fab fa-js-square fa-5x"></i>
+            <i class="fab fa-react fa-5x"></i>
+            </div>
+            <div class="col-12 text-center">
+                <h2 class="skillsSubTitle mt-5">Back-End et outils</h2>
+            </div>
+            <div class="col-12 text-center iconsCol">
+            <i class="fab fa-php fa-5x"></i>
+            <i class="fab fa-symfony fa-5x"></i>
+            <i class="fab fa-wordpress fa-5x"></i>
+            <i class="fab fa-github-square fa-5x"></i>
             </div>
         </div>
     </div>
-    </div>
+    
 </body>
 
 </html>
