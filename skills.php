437e19e7afc36b39f9cf7c865cec36fd7014eb5b
Mathias <mathiascabrol@gmail.com>
2022-02-11 10:34:27 +0100

    Modification du responsive de la page projet

--- skills.php
+++ skills.php
@@ -16,10 +16,10 @@
     <?php include 'nav.php'; ?>
     <div class="container-fluid">
         <div class="row justify-content-center">
-            <div class="col-12 col-md-8 col-lg-8 text-center">
+            <div class="col-12 col-md-8 col-lg-8 col-xl-8 text-center">
                 <img class="avatar" src="../assets/img/Avatar.png">
             </div>
-            <div classe="col-12 col-md-8 col-lg-8">
+            <div classe="col-12 col-md-8 col-lg-8 col-xl-8">
                 <h1 class="skillsTitle">Développeur web</h1>
                 <div class="scrollButton">
                     <a href="#mainContainer" class="scrollIcon">></a>
@@ -27,41 +27,41 @@
             </div>
         </div>
         <div class="row justify-content-center">
-            <div class="normalText col-12 col-md-8 text-center">
+            <div class="normalText col-12 col-md-8 col-lg-8 col-xl-8 text-center">
                 <p>Étant imprégné de la culture numérique depuis mon plus jeune âge, je me suis notamment professionalisé grâce au diplome de développeur web et web mobile, obtenu à l'école "La Manu" de Versailles.</p>
                 <p>Ci-dessous les technos maitrisées ainsi que mon parcours professionnel.</p>
                 <p>Je suis impatient de découvrir le projet que vous représentez !</p>
             </div>
-            <div class="col-12 text-center">
+            <div class="col-12 col-md-12 col-lg-12 col-xl-12 text-center">
                 <h2 class="skillsSubTitle mt-4">Front-End</h2>
             </div>
-            <div class="col-12 text-center iconsCol mb-5">
+            <div class="col-12 col-md-12 col-lg-12 col-xl-12 text-center iconsCol mb-5">
                 <i class="fab fa-html5 fa-5x"></i>
                 <i class="fab fa-css3-alt fa-5x"></i>
                 <i class="fab fa-bootstrap fa-5x"></i>
                 <i class="fab fa-js-square fa-5x"></i>
                 <i class="fab fa-react fa-5x"></i>
             </div>
-            <div class="col-12 text-center">
+            <div class="col-12 col-md-12 col-lg-12 col-xl-12 text-center">
                 <h2 class="skillsSubTitle mt-5">Back-End et outils</h2>
             </div>
-            <div class="col-12 text-center iconsCol">
+            <div class="col-12 col-md-12 col-lg-12 col-xl-12 text-center iconsCol">
                 <i class="fab fa-php fa-5x"></i>
                 <i class="fab fa-symfony fa-5x"></i>
                 <i class="fab fa-wordpress fa-5x"></i>
                 <i class="fab fa-github-square fa-5x"></i>
             </div>
-            <div class="col-8 text-center mt-3">
+            <div class="col-8 col-md-8 col-lg-8 col-xl-8 text-center mt-3">
                 <h2 class="skillsSubTitle mt-5">Formation</h2>
-                <div class="col-12 experienceCol mt-4">
+                <div class="col-12 col-md-12 col-lg-12 col-xl-12 experienceCol mt-4">
                     <p class="organisationName mt-3">Mars 2022</p>
                     <p class="organisationText">Executive Program Developpement web (Titre professionnel : Développement web et web mobile Bac+2 )</p>
                     <p class="organisationName mb-3"><strong>LA MANU, l'Ecole Supérieure des Métiers du Numérique - Versailles</strong></p>
                 </div>
             </div>
-            <div class="col-8 text-center mt-3">
+            <div class="col-8 col-md-8 col-lg-8 col-xl-8 text-center mt-3">
                 <h2 class="skillsSubTitle mt-5">Expériences professionnelles</h2>
-                <div class="col-12 experienceCol mt-4">
+                <div class="col-12 col-md-12 col-lg-12 col-xl-12 experienceCol mt-4">
                     <p class="organisationName mt-4">Mai 2021 - Octobre 2021</p>
                     <p class="organisationText">Agent de réservation</p>
                     <p class="organisationName mb-4"><strong>MGM HÔTELS & RÉSIDENCES - PROMOTEUR IMMOBILIER - MEYTHET</strong></p>
