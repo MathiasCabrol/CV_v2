c395dcb5daf44cbc7dd6afb9cef92a229be22913
Mathias <mathiascabrol@gmail.com>
2022-02-11 09:34:46 +0100

    SUppression du fdossier php

--- index.php
+++ index.php
@@ -6,9 +6,9 @@
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css" integrity="sha512-Fo3rlrZj/k7ujTnHg4CGR2D7kSs0v4LLanw2qksYuRlEzO+tcaEPQogQ0KaoGN26/zrn20ImR1DfuLWnOo7aBA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
-  <link rel="stylesheet" href="../assets/css/slider.css">
-  <link rel="stylesheet" href="../assets/css/nav.css">
-  <link rel="stylesheet" href="../assets/css/footer.css">
+  <link rel="stylesheet" href="assets/css/slider.css">
+  <link rel="stylesheet" href="assets/css/nav.css">
+  <link rel="stylesheet" href="assets/css/footer.css">
   <title>Page d'accueil CV Mathias Cabrol</title>
 </head>
 
@@ -36,6 +36,6 @@
     <a target="blank" href="https://fr.linkedin.com/in/mathias-cabrol-2911b4198"><i class="fab fa-linkedin fa-3x icon"></i></a>
   </footer>
 </body>
-<script src="../assets/js/slider.js"></script>
+<script src="assets/js/slider.js"></script>
 
 </html>
